c30ec47f0eeaece55737c14c0321a435b78085ae
wangdehua <1420598057@qq.com>
2025-07-15 23:34:17 +0800

    第一次向GitHub提交代码

---
+++ demo.py
@@ -0,0 +1,42 @@
+# print("hello world!") # 字符串
+# # print(23333) # 整数
+# print(2.3333) #小数
+# print(True) # 布尔值 True False
+# print(()) # 元组
+# print([]) # 数组
+# print({}) # 字典
+# '''
+# 锄禾日当午
+# 汗滴禾下土
+# '''
+# print("哈哈哈",23333)
+# print("哈哈哈"+"嘘嘘嘘")
+# print(((1+2)*100-9.9)/2)
+# print(2>3)
+
+# a = str(23333)
+# print(type(a))
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("输入的内容：",a+b)
+
+
+a = input('请输入第一段内容：')
+b = input('请输入第二段内容：')
+print('长度是：',len(a+b))
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
